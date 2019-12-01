a2ab50c633c205a9b8ae41686bb51f1aa35c71da
Richard Lee <rlee@tokyo3.com>
2019-12-01 01:19:45 -0800

    finished day 1

--- AdventOfCode2019/ContentView.swift
+++ AdventOfCode2019/ContentView.swift
@@ -9,8 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        let currentPath = FileManager.default.currentDirectoryPath
+        let bundlePath = Bundle.main.resourceURL!
+        
+        // let x = FileManager.default.urls(for: ., in: .userDomainMask)
+        var day1InputFile = bundlePath
+
+
+        do {
+         var y = try FileManager.default.contentsOfDirectory(at: day1InputFile, includingPropertiesForKeys: nil, options: .init(arrayLiteral: .includesDirectoriesPostOrder))
+         var z = 4
+        } catch {
+            
+        }
+
+        
+        //day1InputFile.appendPathComponent("Day1", isDirectory: true)
+        day1InputFile.appendPathComponent("input.txt")
+
+        
+        let i = IntFileIterator(contentsOf: day1InputFile)
+        var sum = 0;
+        for x in i {
+            var xtotal = calculateFuel(ofMass: x)
+            var fuelMass = calculateFuel(ofMass: xtotal)
+            while fuelMass > 0 {
+                xtotal += fuelMass
+                fuelMass = calculateFuel(ofMass: fuelMass)
+            }
+            
+            sum = sum + xtotal
+        }
+
+        
+        return Text("Hello, World! " + String(sum))
     }
 }
 
