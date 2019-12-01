dd7883aa890ec042f2d47ef7371e95036dd62626
Richard Lee <rlee@tokyo3.com>
2019-11-30 22:04:13 -0800

    Initial Commit

---
+++ AdventOfCode2019/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdventOfCode2019
+//
+//  Created by Richard Lee on 11/30/19.
+//  Copyright © 2019 Richard Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
