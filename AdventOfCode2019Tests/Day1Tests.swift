f4330a7b7d77b69e4bc5e1463bc0f1059fd4ece6
Richard Lee <rlee@tokyo3.com>
2019-12-01 23:37:33 -0800

    Renamed the original AdventOfCode2019Tests to be simply Day1Tests

--- AdventOfCode2019Tests/Day1Tests.swift
+++ AdventOfCode2019Tests/Day1Tests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import AdventOfCode2019
 
-class AdventOfCode2019Tests: XCTestCase {
+class Day1Tests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
