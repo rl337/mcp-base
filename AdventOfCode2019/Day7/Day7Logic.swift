481f81c76dbe494597c6e41161c188ffaa9803c3
Richard Lee <rlee@tokyo3.com>
2019-12-06 23:21:32 -0800

    solved part 1 of day 7

---
+++ AdventOfCode2019/Day7/Day7Logic.swift
@@ -0,0 +1,9 @@
+//
+//  Day7Logic.swift
+//  AdventOfCode2019
+//
+//  Created by Richard Lee on 12/6/19.
+//  Copyright © 2019 Richard Lee. All rights reserved.
+//
+
+import Foundation
---
+++ AdventOfCode2019Tests/Day7Tests.swift
@@ -0,0 +1,9 @@
+//
+//  Day7Tests.swift
+//  AdventOfCode2019Tests
+//
+//  Created by Richard Lee on 12/6/19.
+//  Copyright © 2019 Richard Lee. All rights reserved.
+//
+
+import Foundation
